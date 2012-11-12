e46f5473a1dd2f07e9780b8bbb76e25264188987
Ben Zhang <bzhang@thoughtbot.com>
2012-11-11 20:29:23 -0500

    change artist back to talent namespace

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Artist::Application.configure do
+Talent::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
